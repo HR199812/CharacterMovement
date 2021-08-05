7351d4126a122a8b3f27011bf93b53882c0432b2
Hritwik Agarwal <39455725+HR199812@users.noreply.github.com>
2021-08-05 17:40:57 +0530

    Basic View Setup Complete

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var camera, scene, renderer, plane;
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
+ 
+var camera, scene, renderer, plane, orbitControls;
 
 
 function init() {
@@ -8,7 +10,7 @@ function init() {
 
     camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(-180, 250, -150);
-
+    
     let dirlight = new THREE.DirectionalLight(0xFFF000, 1);
     dirlight.position.set(20, 100, 10);
     dirlight.target.position.set(0, 0, 0);
@@ -25,46 +27,45 @@ function init() {
     dirlight.shadow.camera.bottom = -100;
 
     scene.add(dirlight);
-
+    
     // var ambientLight = new THREE.AmbientLight(0xFFFF00, 3);
     // scene.add(ambientLight);
-
+    
     // // let hemiLight = new THREE.HemisphereLight(0xFFFFFF, 0x080820, 4);
     // let hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.1);
     // hemiLight.color.setHSL(0.6, 0.6, 0.6);
     // hemiLight.groundColor.setHSL(0.1, 1, 0.4);
     // hemiLight.position.set(0, 50, 0);
     // scene.add(hemiLight);
-
-
-    plane = new THREE.Mesh(new THREE.PlaneGeometry(15000, 15000, 30, 30),
-        new THREE.MeshBasicMaterial({ color: 0xa0afa4 }));
-
-    // plane.position.set(0, 0, 0);
-    // plane.scale.set(50, 2, 50);
-    // plane.castShadow = true;
-    // plane.receiveShadow = true;
-    scene.add(plane);
-
-    // let blockPlane = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshPhongMaterial({ color: 0xa0afa4 }));
-
-    // blockPlane.position.set(pos.x, pos.y, pos.z);
-    // blockPlane.scale.set(scale.x, scale.y, scale.z);
-
-    // blockPlane.castShadow = true;
-    // blockPlane.receiveShadow = true;
-
-    // scene.add(blockPlane);
-
-    const gridHelper = new THREE.GridHelper(10, 1200);
+    
+    
+    // plane = new THREE.Mesh(new THREE.PlaneGeometry(15000, 15000, 300, 300),
+    //     new THREE.MeshBasicMaterial({ color: 0xa0afa4, wireframe:true }));
+    //     plane.rotation.x = (-Math.PI / 2);
+    // scene.add(plane);
+    
+    
+    const gridHelper = new THREE.GridHelper(15000, 100);
+    gridHelper.background = new THREE.Color(0xa0afa4);
     scene.add(gridHelper);
-
+    
     renderer = new THREE.WebGLRenderer({ antialiasing: true });
     renderer.toneMapping = THREE.ReinhardToneMapping;
     renderer.toneMappingExposure = 2.3;
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+
+    orbitControls = new OrbitControls(camera, renderer.domElement);
+    orbitControls.enableDamping = true;
+    orbitControls.enableZoom = true;
+    orbitControls.zoomSpeed = 1.15;
+    orbitControls.screenSpacePanning = false;
+    orbitControls.minDistance = 0;
+    orbitControls.maxDistance = 45000;
+    orbitControls.minPolarAngle = -Math.PI / 1.5;
+    orbitControls.maxPolarAngle = Math.PI / 2.5;
+    orbitControls.update();
 
     // renderer.render(scene, camera);
 
@@ -75,16 +76,17 @@ function animate() {
 
     requestAnimationFrame(animate);
 
+    orbitControls.update();
     renderer.render(scene, camera);
 }
 
 init();
 
 function onWindowResize() {
-    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix();
 
-    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 window.addEventListener('resize', onWindowResize);
